d1c98d6627ca8deb2ffcc9c7ee4671b573052ed9
Michelle Sousa Cordeiro <mic321@gmail.com>
2024-07-26 16:43:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -81,8 +81,8 @@ var available_models = {
     ],
     model: null,
   },
-  "My YOLOv8": {
-    name: "alphabet_card-2/2",
+  "alphabet_card-2/2": {
+    name: "My YOLOv8",
     version: 2,
     confidence: 0.3,
     video:
